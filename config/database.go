ee4db50b811f0d0b406a388ca07294b53000f5b9
Rajih kharissuha <rajhk704@gmail.com>
2025-12-06 10:46:14 +0700

    Update Config Database

--- config/database.go
+++ config/database.go
@@ -1,7 +1,6 @@
 package config
 
 import (
-	"fmt"
 	"log"
 	"os"
 
@@ -12,18 +11,19 @@ import (
 var DB *gorm.DB
 
 func ConnectDatabase() {
-	host := os.Getenv("DB_HOST")
-	user := os.Getenv("DB_USER")
-	password := os.Getenv("DB_PASSWORD")
-	dbname := os.Getenv("DB_NAME")
-	port := os.Getenv("DB_PORT")
-
-	dsn := fmt.Sprintf("host=%s user=%s password=%s dbname=%s port=%s sslmode=disable",
-		host, user, password, dbname, port)
-
-	var err error
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
-	if err != nil {
-		log.Fatal("Gagal connect database:", err)
+	dsn := os.Getenv("DATABASE_URL") // Railway memberikan ini
+	if dsn == "" {
+		log.Fatal("DATABASE_URL tidak ditemukan")
 	}
+
+	// Railway butuh sslmode=require agar connect
+	dsn = dsn + " sslmode=require"
+
+	db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{})
+	if err != nil {
+		log.Fatal("Gagal connect ke database:", err)
+	}
+
+	DB = db
+	log.Println("Database connected")
 }
